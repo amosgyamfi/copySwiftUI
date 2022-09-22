c9e9769b19059d0d8e2da2fe13cfb6afb8c23333
Amos Gyamfi <amos.gyamfi@getstream.io>
2022-09-23 01:48:55 +0300

    Initial Commit

---
+++ iOS16ScreensIsland/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  iOS16ScreensIsland
+//
+//  Created by amos.gyamfi@getstream.io on 23.9.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ iOS16ScreensIsland/iOS16ScreensIslandApp.swift
@@ -0,0 +1,17 @@
+//
+//  iOS16ScreensIslandApp.swift
+//  iOS16ScreensIsland
+//
+//  Created by amos.gyamfi@getstream.io on 23.9.2022.
+//
+
+import SwiftUI
+
+@main
+struct iOS16ScreensIslandApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
